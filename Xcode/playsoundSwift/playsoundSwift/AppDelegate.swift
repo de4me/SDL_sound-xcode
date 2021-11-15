a9e3e4b8fa5e548a2bc3fc464862cc2312e546ce
Denis Popov <info@de4me.com>
2021-11-15 14:18:10 +0300

    Remove unneeded includes;

--- Xcode/playsoundSwift/playsoundSwift/AppDelegate.swift
+++ Xcode/playsoundSwift/playsoundSwift/AppDelegate.swift
@@ -9,29 +9,6 @@ import Cocoa;
 import SDL;
 import SDL_Sound;
 
-import SDL.OpenGL
-import SDL.SDL_active
-import SDL.SDL_audio
-import SDL.SDL_cdrom
-import SDL.SDL_cpuinfo
-import SDL.SDL_endian
-import SDL.SDL_error
-import SDL.SDL_events
-import SDL.SDL_joystick
-import SDL.SDL_keyboard
-import SDL.SDL_keysym
-import SDL.SDL_loadso
-import SDL.SDL_main
-import SDL.SDL_mouse
-import SDL.SDL_mutex
-import SDL.SDL_rwops
-import SDL.SDL_stdinc
-import SDL.SDL_thread
-import SDL.SDL_timer
-import SDL.SDL_version
-import SDL.SDL_video
-import SDL.SysWM
-
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
